1ef278a620356de07c686d078431275ba2cd2813
root <root@t2.freqai.pro-1773372284248-s-4vcpu-8gb-blr1-01>
2026-03-26 11:41:58 +0000

    fix: remove duplicate Image import in admin-image-upload

--- frontend/components/admin-interface/components/admin-image-upload.tsx
+++ frontend/components/admin-interface/components/admin-image-upload.tsx
@@ -2,7 +2,6 @@
 
 import type React from "react";
 import Image from "next/image";
-import Image from "next/image";
 import { useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
